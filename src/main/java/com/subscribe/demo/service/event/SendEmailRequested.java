7d226df6396d618d1c239e520391192047806526
Lahatranomena <hei.lahatra.3@gmail.com>
2026-07-11 14:08:57 +0300

    style: add format to SendEmailRequested

--- src/main/java/com/subscribe/demo/service/event/SendEmailRequested.java
+++ src/main/java/com/subscribe/demo/service/event/SendEmailRequested.java
@@ -1,8 +1,7 @@
 package com.subscribe.demo.service.event;
 
-import java.time.Duration;
-
 import com.subscribe.demo.endpoint.event.model.PojaEvent;
+import java.time.Duration;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,16 +16,15 @@ import lombok.ToString;
 @EqualsAndHashCode(callSuper = false)
 @ToString
 public class SendEmailRequested extends PojaEvent {
-    private String to;
+  private String to;
 
-    @Override
-    public Duration maxConsumerDuration() {
-        return Duration.ofSeconds(45);
-    }
+  @Override
+  public Duration maxConsumerDuration() {
+    return Duration.ofSeconds(45);
+  }
 
-    @Override
-    public Duration maxConsumerBackoffBetweenRetries() {
-        return Duration.ofSeconds(30);
-    }
+  @Override
+  public Duration maxConsumerBackoffBetweenRetries() {
+    return Duration.ofSeconds(30);
+  }
 }
-
