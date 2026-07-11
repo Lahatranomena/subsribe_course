9a834ec76ced0d4db9bcf318f5f3d094ae8a5348
Lahatranomena <hei.lahatra.3@gmail.com>
2026-07-11 14:32:45 +0300

    feat(entity): add class User

---
+++ src/main/java/com/subscribe/demo/subscribe/entity/User.java
@@ -0,0 +1,31 @@
+package com.subscribe.demo.subscribe.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "app_user")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "firstname", nullable = false)
+    private String firstname;
+
+    @Column(name = "lastname", nullable = false)
+    private String lastname;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+}
